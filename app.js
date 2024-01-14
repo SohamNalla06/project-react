291527099993802b52a52a72e48289edb33c33e1
SohamNalla06 <Sohamhi06@gmail.com>
2024-01-14 12:10:05 +0530

    esp-01

---
+++ app.js
@@ -0,0 +1,7 @@
+<script>
+    const heading = React.createElement("h1", {}, "Hello World from React");
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
+</script>
+
+const parent = React 
